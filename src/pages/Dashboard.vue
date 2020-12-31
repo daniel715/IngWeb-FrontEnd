3dbc5096e3526262c9e54300c5addee8d6e1d385
daniel715 <danielmontellanosllamoca@gmail.com>
2020-12-30 22:34:36 -0500

    planilla de front 1%

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,19 @@
+<style lang="sass" scoped>
+
+</style>
+
+
+<template>
+  <div class="container" >
+      <div class="padre" >
+          <h1>Ventas</h1>
+          <button class="md-dense" > asdasd</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
